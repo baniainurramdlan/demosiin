06ca50895d66e9fb36776090c5a35544df484027
abyanjksatu <abyanjksatu@gmail.com>
2017-01-20 20:54:02 -0800

    center

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -6,24 +6,28 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
+<div class="row">
+  <div class="col-md-12 col-centered">
     <div class="jumbotron">
         <?=$map->display()?>
     </div>
+  </div>
+</div>
 
     <div class="container">
 
         <div class="row">
             <div class="col-md-3">
-                <p><a class="btn btn-default" href="<?php echo \yii\helpers\Url::to(['/budget']); ?>">Budget &raquo;</a></p>
+                <center><a class="btn btn-default" href="<?php echo \yii\helpers\Url::to(['/budget']); ?>">Budget &raquo;</a></center>
             </div>
             <div class="col-md-3">
-                <p><a class="btn btn-default" href="<?php echo \yii\helpers\Url::to(['/sdm']); ?>">SDM &raquo;</a></p>
+                <center><a class="btn btn-default" href="<?php echo \yii\helpers\Url::to(['/sdm']); ?>">SDM &raquo;</a></center>
             </div>
             <div class="col-md-3">
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Lembaga &raquo;</a></p>
+                <center><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Lembaga &raquo;</a></center>
             </div>
             <div class="col-md-3">
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Output &raquo;</a></p>
+                <center><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Output &raquo;</a></center>
             </div>
         </div>
 
