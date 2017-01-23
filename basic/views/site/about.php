f0aa2b5820412cb0902e45c46450acbd0e50f21d
abyanjksatu <abyanjksatu@gmail.com>
2017-01-23 11:13:04 -0800

    about

--- basic/views/site/about.php
+++ basic/views/site/about.php
@@ -4,15 +4,31 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'About Sistem Informasi Iptek Nasional';
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <!--
     <p>
         This is the About page. You may modify the following file to customize its content:
     </p>
 
     <code><?= __FILE__ ?></code>
+  -->
+
+  <p>
+    Data dan informasi iptek untuk pengawasan dan perencanaan pembangunan iptek serta dapat dibandingkan dengan negara lain perlu gambaran kondisi iptek secara nasional.
+  </p>
+  <p>
+    Data dan informasi iptek yang memenuhi kebutuhan informasi pada:
+    <ul>
+        <li>UU No. 18 tahun 2002,</li>
+        <li>RUU pengganti UU No. 18 tahun 2002,</li>
+        <li>Renstra Kemenristekdikti tahun 2015-2019,</li>
+        <li>Frascatti Manual 2015.</li>
+      </ul>
+  </p>
+
 </div>
