aff3c00522516a4eccf5591b5b885986b3faf1d8
vinni-150 <viniapriliani5@gmail.com>
2017-01-23 13:24:19 +0700

    admin

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=demosiin',
+    'dsn' => 'mysql:host=new-dacdev;dbname=siin',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
