d44ba532587b006ffd00925edc641759174fbf13
Bani Ramdlan <baniramdlan@gmail.com>
2017-01-23 09:45:00 +0700

    layout

--- basic/controllers/AdminController.php
+++ basic/controllers/AdminController.php
@@ -4,8 +4,11 @@ namespace app\controllers;
 
 class AdminController extends \yii\web\Controller
 {
+    
     public function actionIndex()
     {
+        $this->layout = 'main_admin';
+        
         return $this->render('index');
     }
 
