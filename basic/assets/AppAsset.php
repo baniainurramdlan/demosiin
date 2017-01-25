b2ab1679b0a29883c770a41e185251c1bc68833a
abyanjksatu <abyanjksatu@gmail.com>
2017-01-25 08:23:51 -0800

    navbar fix

--- basic/assets/AppAsset.php
+++ basic/assets/AppAsset.php
@@ -18,9 +18,27 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css',
+        'css/AdminLTE.min.css',
+        'css/jquery-jvectormap-1.2.2.css',
+        'css/_all-skins.min.css',
+        'css/blue.css',
+        'css/site.css',
         'css/site.css',
     ];
     public $js = [
+                'js/jquery-2.2.3.min.js',
+        'js/bootstrap.min.js',
+        'js/fastclick.min.js',
+        //'js/app.min.js',
+        'js/jquery.sparkline.min.js',
+        'js/jquery-jvectormap-1.2.2.min.js',
+        'js/jquery-jvectormap-world-mill-en.js',
+        'js/jquery.slimscroll.min.js',
+        'js/Chart.min.js',
+        //'js/dashboard2.js',
+        'js/demo.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
